3702e4d864862c3bbff376c05472fe34a88658b4
JoheoungWoo <106474486+JoheoungWoo@users.noreply.github.com>
2025-10-28 18:19:10 +0900

    @Transactional 추가
    Tests에 @Transactional빠져서 수정하였습니다.

--- src/test/java/com/example/demo/repository/CourseTests.java
+++ src/test/java/com/example/demo/repository/CourseTests.java
@@ -2,11 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Course;
 import com.example.demo.entity.User;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Transactional
 public class CourseTests {
     @Autowired
     CourseRepository repository;
